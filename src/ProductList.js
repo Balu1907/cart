211ff459692b2a9f584c06512627db67097622fb
balu <balu@ghoshak.com>
2023-03-27 11:13:51 +0530

    cart

---
+++ src/ProductList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProductList() {
+  return (
+    <div>
+      <h1> kvhfihvudhuvhuvhuhvjhuohu hgigriushfiu</h1>
+    </div>
+  );
+}
+
+export default ProductList;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import ProductList from "./ProductList";
+
+ReactDOM.render(
+  <ProductList />,
+
+  document.querySelector("#cart-dev-root")
+);
